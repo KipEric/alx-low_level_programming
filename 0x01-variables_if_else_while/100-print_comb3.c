b1b26ab37111d8f55d0f7986fabfe403beecac37
KipEric <kipkoecheric97@gmail.com>
2022-09-10 01:25:33 -0700

    My task 10 commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits.
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+	int x = '0';
+
+	while (i <= '9')
+	{
+		while (x <= '9')
+		{
+			putchar(i);
+			putchar(x);
+			if (x == '9' && i == '9');
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			x++;
+		}
+		if (x >= '9')
+		{
+			x = '0';
+		}
+		i++;
+	}
+	return (0);
+}
