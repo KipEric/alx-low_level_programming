5ff2beddd5d2bef0b8b744c112b23cb7172621ee
KipEric <kipkoecheric97@gmail.com>
2022-09-10 00:26:22 -0700

    My task 4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase.
+ * Return: 0
+ */
+int main (void)
+{
+	char b = 'a';
+
+	while (b <= 'z')
+	{
+		if (b != 'q' && b !='e')
+		{ 
+			putchar(b);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
