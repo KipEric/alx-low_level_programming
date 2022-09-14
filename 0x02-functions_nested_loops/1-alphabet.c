5874e9ed1d4d120b5c90f3023c7fd0fb51b0f0bf
KipEric <kipkoecheric97@gmail.com>
2022-09-14 10:31:16 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+ /**
+  * main - print alphabet
+  * Return: 0
+  */
+void print_alphabet(void)
+{
+	char b;
+	
+	for (b = 'a'; b <= 'z'; b++)
+		_putchar(b);
+
+	_putchar('\n');
+}
