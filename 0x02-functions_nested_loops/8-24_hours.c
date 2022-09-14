ea1e9a78e25d562b4d5fc451248bc760a6704163
KipEric <kipkoecheric97@gmail.com>
2022-09-14 14:24:36 -0700

    My task 8 commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,26 +2,49 @@
 
 /*
  *
- * main - 2-print_alphabet_x10.c
+ * _printstr - prints a given string
+ * @c: a null temrinated character array
  *
  * Return: 0
  *
  */
 
+int _printstr(char c[])
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+		_putchar(c[i++]);
+
+	return (0);
+}
+
+/**
+ * jack_bauer - prints times from 00:00 to 24:00
+ * Return: 0 on completion
+ */
 void jack_bauer(void)
 {
-	int i, j;
+	char x[6] = "00:00\n";
 
-	for (i = 0; i <= 23; i++)
+	while (x[0] != '2' ||  x[1] != '4')
 	{
-		for (j = 0; j <= 59; j++)
+		_printstr(x);
+		x[4]++;
+		if (x[4] == '9' + 1)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':);
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
+			x[4] = '0';
+			x[3]++;
+		}
+		if (x[3] == '6')
+		{
+			x[3] = '0';
+			x[1]++;
+		}
+		if (x[1] == '9' + 1)
+		{
+			x[1] = '0';
+			x[0]++;
 		}
 	}
 }
