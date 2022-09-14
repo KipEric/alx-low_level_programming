d25c660e5e3f91bf9d6b25ab48239128c6fff6a6
KipEric <kipkoecheric97@gmail.com>
2022-09-14 12:30:54 -0700

    My task 9 commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/*
+ *
+ * times_table - Prints the 9 times table, starting with 0.
+ * Description: Learn your times table.
+ * Return: 0
+ *
+ */
+void times_table(void)
+{
+	int i, mult, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = i * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
