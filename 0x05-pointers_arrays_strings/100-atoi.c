731be4d57d43b840f5097de4e3afb9bfc59480a0
KipEric <kipkoecheric97@gmail.com>
2022-09-19 13:30:56 -0700

    My task 10 commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,26 +7,30 @@
  */
 int _atoi(char *s)
 {
-	int signs = 0;
-	unsigned int sum= 0;
-	int final = 0;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	for (; *s != '\0'; s++)
+	while (s[c])
 	{
-		if (*s == '-')
-			signs *= -1;
-		else if (*s >= '0' && *s <= '9')
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (isi == 1)
+		{
 			break;
+		}
+		c++;
 	}
-	while (*s >= '0' && *s <= '9')
-	{
-		sum *= 10;
-		sum += *s - '0';
-		s++;
-	}
-	if (signs == -1)
-		final = -sum;
-	else
-		final = sum;
-	return (final);
+	ni *= min;
+	return (ni);
 }
