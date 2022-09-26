87c2ce3a9efcba98519195c0a5ed1c69ed69f53a
KipEric <kipkoecheric97@gmail.com>
2022-09-26 09:14:17 -0700

    My task 1 commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
 #include "main.h"
-/**
- * _memcpy - copies n bytes from source memory to destination
- *
- * @dest: destination to write to
- * @src: source to copy from
- * @n: number of bytes to write
- *
- * Return: pointer to dest
- */
-char *_memcpy(char *dest, char *src, int n)
-{
-	char *ptr = dest;
 
-	while (n--)
-		*ptr++ = *src++;
+/**
+  * _memcpy - copy memory area
+  * @dest: dest memory area
+  * @src: source memory area
+  * @n: bytes from memory area to copy
+  *
+  * Return: memory area replaced
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
 
 	return (dest);
 }
