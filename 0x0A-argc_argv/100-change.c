4c337538a5e777c6d1ee5ab3588eef313c0ff067
KipEric <kipkoecheric97@gmail.com>
2022-09-30 08:30:37 -0700

    My task 4 commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * main - Prints the sum of args positive numbers
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	int n, i, result = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i < 5 && n >= 0; i++)
+	{
+		while (n >= coins[i])
+		{
+		n -= coins[i];
+		result++;
+		}
+	}
+	printf("%d\n", result);
+}
