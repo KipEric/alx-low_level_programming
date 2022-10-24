e9537acc045b0139dc211e81a406094ee28a961f
KipEric <kipkoecheric97@gmail.com>
2022-10-24 06:32:41 -0700

    My task 0 commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,18 @@
 
 size_t print_list(const list_t *h)
 {
+	size_t i = 0;
 
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		h = h->next;
+		i++;
 	}
-	return (h);
+	return (i);
 }
 
