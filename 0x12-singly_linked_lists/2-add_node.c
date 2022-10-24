456ecbeb8cd6957c57db2c87a5eb992eedd4ecf4
KipEric <kipkoecheric97@gmail.com>
2022-10-24 07:11:27 -0700

    My task 2 commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,40 +12,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp;
+	size_t n;
 
-	if (head != NULL && str != NULL)
-	{
-		temp = malloc(sizeof(list_t));
-		if (temp == NULL)
-			return (NULL);
+	temp = malloc(sizeof(list_t));
+	if (temp ==NULL)
+		return (NULL);
+	temp->str = strdup(str);
 
-		temp->str = strdup(str);
-		temp->len = _strlen(str);
-		temp->next = *head;
+	for (n = 0; str[n]; n++)
+		;
 
-		*head = temp;
+	temp->len = n;
+	temp->next = *head;
+	*head = temp;
 
-		return (temp);
-	}
-
-	return (0);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (*s)
-	{
-		s++;
-		c++;
-	}
-
-	return (c);
+	return (*head);
 }
