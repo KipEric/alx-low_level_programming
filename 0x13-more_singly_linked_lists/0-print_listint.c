9cc532ef6b899dc2897d40071099af7da1c439d6
KipEric <kipkoecheric97@gmail.com>
2022-10-27 09:53:58 -0700

    My task 0 commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,13 +12,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t len;
+	const listint_t *ptr = h;
+	size_t c = 0;
 
-	if (h == NULL)
-		return (0);
-
-	for (len = 0; h != NULL; h = h->next, len++)
-		printf("%d\n", h->n);
-
-	return (len);
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		c++;
+		ptr = ptr->next;
+	}
+	return (c);
 }
