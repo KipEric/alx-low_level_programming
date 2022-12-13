086ba656861dbe37ffcdebb42162ff6e3f92e9c3
KipEric <kipkoecheric97@gmai.com>
2022-12-13 00:42:50 -0800

    My dynamic library commit

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ *
+ * _abs - 2-print_alphabet_x10.c
+ * Description: Function that computes the absolute value
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
