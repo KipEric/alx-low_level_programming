9444a1837b3923a0a936f4947503c8b5c89534c8
KipEric <kipkoecheric97@gmai.com>
2022-12-13 01:37:24 -0800

    My puts commit

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 /**
  * _puts - prints a string, followed by a new line.
- * @str: input string to print.
+ * @s: input string to print.
  *
  * Description: print a string
  * Return: no error
  */
-void _puts(char *str)
+void _puts(char *s)
 {
 	int i = 0;
 
-	while (*(str + i) != '\0')
+	while (*(s + i) != '\0')
 	{
-		putchar(*(str + i));
+		putchar(*(s + i));
 		i++;
 	}
 	putchar(10);
